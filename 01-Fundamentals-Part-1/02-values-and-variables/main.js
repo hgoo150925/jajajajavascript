9b823f4eb46931f3fa34e849fac816db9ae2c8bd
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-05 22:37:13 -0300

    values & variables

---
+++ 01-Fundamentals-Part-1/02-values-and-variables/main.js
@@ -0,0 +1,11 @@
+// valor es basicamente una pieza de dato
+// 'Jonas' es un valor
+console.log("Jonas");
+
+// Los valores podemos guardarlos en variables para poder reutilizarlos las veces que sean necesarias
+// La variable (firstName) almacena el valor "Matilda"
+let firstName = "Matilda";
+console.log(firstName);
+
+let fullName = "Ryan Gosling";
+console.log(fullName);
